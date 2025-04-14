e3f80298eeeeaa7ee12421e5b153a2e78f6dcc9b
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-14 20:26:51 +0200

    feat: `publishScheduledContest` job

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log('hello world')
+import { CronJob } from 'cron'
+import { env } from './lib/env'
+import { tryCatch } from './lib/try-catch'
+
+const name = 'publishScheduledContest'
+const cronTime = '35 20 * * *'
+// const cronTime = '0 20 * * SAT'
+const timeZone = 'Europe/Berlin'
+CronJob.from({
+  cronTime,
+  onTick: async () => {
+    console.log(`Running ${name} tick`)
+    const { data, error } = await tryCatch(
+      fetch(env.CONTEST_CREATION_WEBHOOK_URL, {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${env.CONTEST_CREATION_WEBHOOK_SECRET}`,
+        },
+      }),
+    )
+    if (error) console.error(error.message)
+    else console.log(await data.text())
+    console.log('\n')
+  },
+  start: true,
+  timeZone,
+})
+console.log(`Cron job ${name} (${cronTime}, ${timeZone}) started\n`)
